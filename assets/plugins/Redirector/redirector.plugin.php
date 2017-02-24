e74f74053bfc85939fe6bb291eeb86659a160ec8
Andrej Kabachnik <andrej_ka@gmx.de>
2017-02-24 14:07:10 +0100

    Initial release

---
+++ assets/plugins/Redirector/redirector.plugin.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Redirector
+ *
+ * Redirects from http://modx.com/167 to page with ID=167 regardles of the user friendly URL settings
+ *
+ * @category    plugin
+ * @version     0.1
+ * @documentation Readme https://github.com/kabachello/modx.plugin.Redirector/blob/master/README.md
+ * @internal    @events OnPageNotFound
+ * @internal    @modx_category Navigation
+ * @author		kabachello
+ * @lastupdate  24/01/2017
+ */
+switch ($modx->Event->name) {
+	case "OnPageNotFound":
+		$params = '';
+		$addr = substr($_SERVER['REQUEST_URI'], strrpos($_SERVER['REQUEST_URI'], '/')+1);
+		if($params_start = strpos($addr, '?')){
+			$params = substr($addr, $params_start+1);
+			$addr = substr($addr, 0, $params_start);	
+		}
+		if (is_numeric($addr)){
+			$to = $modx->makeUrl(intval($addr), '', $params, 'full');
+			return $modx->sendRedirect($to,0,'REDIRECT_HEADER','HTTP/1.1 301 Moved Permanently');
+		} 
+		break;
+
+	default: break;
+}
